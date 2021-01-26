f750e5d49ce705b9f8ed8656e820dd0eaa63f86f
Cleberson F R Junior <cleberson.dev@gmail.com>
2021-01-25 23:45:28 -0300

    Assembled app's core components

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,51 @@
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import Header from "./components/Header";
+import Notification from "./components/Notification";
+import SearchArea from "./components/SearchArea";
+import CardArea from "./components/CardArea";
+import { fetchCountries } from "./slices/countriesSlice";
+import { fetchPlaces } from "./slices/placesSlice";
+import { notify } from "./slices/notificationSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetch("https://restcountries.eu/rest/v2/all")
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch(
+          fetchCountries(
+            data.map((c: any) => ({
+              id: c.alpha3Code,
+              name: c.translations.pt,
+              flag: c.flag,
+            }))
+          )
+        );
+        return fetch("http://localhost:5000/places");
+      })
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch(fetchPlaces(data));
+      })
+      .catch((err) => {
+        console.error(err);
+        dispatch(
+          notify({ type: "error", message: "Erro interno no servidor =(" })
+        );
+      });
+  }, [dispatch]);
+
   return (
     <div>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6">
-            Lugares
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Container>
-        Olá, mundo!
-      </Container>
+      <Header />
+
+      <SearchArea />
+      <CardArea />
+
+      <Notification />
     </div>
   );
 }
