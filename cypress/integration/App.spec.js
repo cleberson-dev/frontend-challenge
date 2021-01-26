f12826ee889ed88e23933a8a1de167b195f7bcad
Cleberson F R Junior <cleberson.dev@gmail.com>
2021-01-26 18:09:12 -0300

    Refactored App.spec.js

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -1,17 +1,7 @@
 before(() => {
-  cy.visit('http://localhost:3000/');
+  cy.visit("http://localhost:3000");
 });
 
-it('should render correctly', () => {
-  cy.get('header').contains('Lugares que quero conhecer');
+it("should render correctly", () => {
+  cy.get("header").contains("Lugares que quero conhecer");
 });
-
-// it('should add a new place', () => {
-//   cy.get('select').select('BRA');
-//   cy.get('input')
-//     .first().type('São Luís')
-//     .parent().next().type('022021');
-//   cy.get('button').first().click();
-
-  
-// });
