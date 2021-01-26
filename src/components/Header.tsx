3d4c3b5305c13698a3c352a4d59405f6b536bbe1
Cleberson F R Junior <cleberson.dev@gmail.com>
2021-01-26 20:13:43 -0300

    Made app's logo responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,21 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import logo from "../img/logo.png";
+import styled from 'styled-components';
+
+const Logo = styled.img`
+  height: 75px;
+
+  @media(max-width: 741px) {
+    height: 60px;
+  }
+`;
 
 export default function Header() {
   return (
     <AppBar color="transparent" position="static">
       <Toolbar style={{ background: 'black' }}>
-        <img src={logo} alt="Logotipo" height="75px" />
+        <Logo src={logo} alt="Logotipo" />
         <Typography sx={{ display: 'none' }} variant="h6">Lugares que quero conhecer</Typography>
       </Toolbar>
     </AppBar>
