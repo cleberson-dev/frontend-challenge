90e564cb0142ea2976a5717849ae1f04a0cb38e9
Cleberson F R Junior <cleberson.dev@gmail.com>
2021-01-26 19:58:25 -0300

    Changed AppBar to match requirements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import logo from "../img/logo.png";
 
 export default function Header() {
   return (
-    <AppBar color="secondary" position="static">
-      <Toolbar>
-        <Typography variant="h6">Lugares que quero conhecer</Typography>
+    <AppBar color="transparent" position="static">
+      <Toolbar style={{ background: 'black' }}>
+        <img src={logo} alt="Logotipo" height="75px" />
+        <Typography sx={{ display: 'none' }} variant="h6">Lugares que quero conhecer</Typography>
       </Toolbar>
     </AppBar>
   );
