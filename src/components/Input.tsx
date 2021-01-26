7c56487730f19f0aa9d5706426c7d8b22db064c7
Cleberson F R Junior <cleberson.dev@gmail.com>
2021-01-25 23:15:01 -0300

    Created Input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default styled.input`
+  padding: 1rem;
+  border-radius: 7px;
+  border: none;
+  outline: none;
+  font-size: 16px;
+  width: 100%;
+  box-sizing: border-box;
+`;
