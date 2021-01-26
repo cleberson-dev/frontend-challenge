5b91e7f91271195d71aca4d522fe743e9277c2c1
Cleberson F R Junior <cleberson.dev@gmail.com>
2021-01-26 18:11:15 -0300

    Added test case 'should add a new place'

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -12,3 +12,24 @@ it("should format the goal input MM/YYYY", () => {
   .should("have.value", "11/1999")
   .clear();
 });
+
+function addNewPlace(countryCode, local, typedGoalDate) {
+  cy.get('label').contains(/país/i).parent().find('select')
+    .select(countryCode);
+  cy.get('label').contains(/local/i).parent().find('input')
+    .type(local);
+  cy.get('label').contains(/meta/i).parent().find('input')
+    .type(typedGoalDate);
+  cy.get('form button[type="submit"]').click();
+}
+
+it("should add a new place", () => {
+  addNewPlace('BRA', 'São Luís', '022021');
+
+  cy.get("ul li").should("have.length", 1);
+  cy.get("ul li")
+    .first()
+    .should("contain", "Brasil")
+    .and("contain", "São Luís")
+    .and("contain", "02/2021");
+});
